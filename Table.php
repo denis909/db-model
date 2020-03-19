bb4fd65ed56fca84c805e17c49781647e7fd4d9f
denis909 <denis909@mail.ru>
2020-03-19 13:10:54 +0300

    n/a

--- Table.php
+++ Table.php
@@ -1,30 +1,32 @@
 <?php
 
-namespace denis909\db;
+namespace Denis909\Db;
 
 use Exception;
 
-class Table
+abstract class Table
 {
     
-    public $tableName;
-
-    public $primaryKey;
-
     protected $_db;
 
-    public function __construct($db)
+    public function __construct(Db $db)
     {
         $this->_db = $db;
     }
 
+    abstract static function tableName();
+
+    abstract static function primaryKey();
+
     public function getPrimaryKey($data)
     {
-        if (is_array($this->primaryKey))
+        $primaryKey = static::primaryKey();
+
+        if (is_array($primaryKey))
         {
             $return = [];
 
-            foreach($this->primaryKey as $column)
+            foreach($primaryKey as $column)
             {
                 if (!$data->$column)
                 {
@@ -37,7 +39,7 @@ class Table
             return $return;
         }
         
-        $return = $data->{$this->primaryKey};
+        $return = $data->{$primaryKey};
 
         if (!$return)
         {
@@ -49,27 +51,29 @@ class Table
 
     public function wherePrimaryKey($pk)
     {
+        $primaryKey = static::primaryKey();
+
         if (is_array($pk))
         {
             return $pk;
         }
 
-        if (is_array($this->primaryKey))
+        if (is_array($primaryKey))
         {
-            throw new Exception('Primary key is not valid.');
+            throw new Exception('Primary key not valid.');
         }
 
-        return [$this->primaryKey => $pk];
+        return [$primaryKey => $pk];
     }
 
     public function findByPk($pk)
     {
-        return $this->_db->findOne($this->tableName, $this->wherePrimaryKey($pk));
+        return $this->_db->findOne(static::tableName(), $this->wherePrimaryKey($pk));
     }
 
     public function insert($data)
     {
-        return $this->_db->insert($this->tableName, (array) $data);
+        return $this->_db->insert(static::tableName(), (array) $data);
     }
 
     public function update($data)
@@ -78,11 +82,10 @@ class Table
 
         if (!$pk)
         {
-            throw new Exception('Primary key is not defined.');
-
+            throw new Exception('Primary key not defined.');
         }
 
-        return $this->_db->update($this->tableName, (array) $data, $this->wherePrimaryKey($pk));
+        return $this->_db->update(static::tableName(), $data, $this->wherePrimaryKey($pk));
     }    
 
     public function save($data)
@@ -99,26 +102,26 @@ class Table
         }
     }
 
-    public function delete($data)
+    public function delete($data) : bool
     {
-        $pk = $this->getPrimaryKey($data);
+        $pk = $this->primaryKey($data);
 
         if (!$pk)
         {
             throw new Exception('Primary key is not defined.');
         }
 
-        return $this->_db->delete($this->tableName, $this->wherePrimaryKey($pk));
+        return $this->_db->delete(static::tableName(), $this->wherePrimaryKey($pk));
     }
 
-    public function findAll($where = null, $params = [], $suffix = null)
+    public function findAll($where = null, $params = [], $suffix = null) : array
     {
-        return $this->_db->findAll($this->tableName, $where, $params, $suffix);
+        return $this->_db->findAll(static::tableName(), $where, $params, $suffix);
     }
 
     public function findOne($where = null, $params = [], $suffix = '')
     {
-        return $this->_db->findOne($this->tableName, $where, $params, $suffix);
-    }    
+        return $this->_db->findOne(static::tableName(), $where, $params, $suffix);
+    }
     
 }
